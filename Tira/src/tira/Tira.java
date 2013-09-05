c392a379e382c978dffb486b01b2dff0230e5100
Petri M Makinen <pcmakine@dhp-11.cs.helsinki.fi>
2013-09-05 13:28:00 +0300

    tyhjä projekti

---
+++ Tira/src/tira/Tira.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tira;
+
+/**
+ *
+ * @author pcmakine
+ */
+public class Tira {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
